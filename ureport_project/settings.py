7e330ef6510c9c8df0fc045c69c616332c20728f
David McCann <david.a.mccann@gmail.com>
2011-09-12 08:56:54 +0300

    site id to 1

--- ureport_project/settings.py
+++ ureport_project/settings.py
@@ -175,7 +175,7 @@ ADMIN_MEDIA_PREFIX = "/static/media/"
 # this is required for the django.contrib.sites tests to run, but also
 # not included in global_settings.py, and is almost always ``1``.
 # see: http://docs.djangoproject.com/en/dev/ref/contrib/sites/
-SITE_ID = 2
+SITE_ID = 1 
 
 
 # the default log settings are very noisy.

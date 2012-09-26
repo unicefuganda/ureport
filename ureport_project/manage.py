b274e37e513905bdd808f15f12c4ad4438093e7f
Mugisha Moses <mossplix@gmail.com>
2012-09-26 14:05:13 +0300

    revert to multiprocessing

--- ureport_project/manage.py
+++ ureport_project/manage.py
@@ -3,13 +3,6 @@
 
 from django.core.management import execute_manager
 import settings
-import eventlet
-import os
-import eventlet.debug
-
-os.environ["EVENTLET_NOPATCH"] = 'True'
-eventlet.monkey_patch()
-eventlet.debug.hub_prevent_multiple_readers(False)
 
 if __name__ == "__main__":
     execute_manager(settings)

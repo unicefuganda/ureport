6fffb469db1d69324da7789551fbf0b908275832
Mugisha Moses <mossplix@gmail.com>
2012-11-04 01:20:12 +0300

    get rid of eventlet

--- ureport_project/manage.py
+++ ureport_project/manage.py
@@ -3,13 +3,5 @@
 
 from django.core.management import execute_manager
 import settings
-import eventlet
-import eventlet.debug
-import os
-#patch for eventlet
-os.environ["EVENTLET_NOPATCH"] = 'True'
-eventlet.monkey_patch()
-eventlet.debug.hub_prevent_multiple_readers(False)
-
 if __name__ == "__main__":
     execute_manager(settings)

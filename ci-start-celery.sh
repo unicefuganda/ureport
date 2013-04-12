44bc61dd60854c2ce5f19630a1ea367241295e5a
Jim Barritt <jim@planet-ix.com>
2013-04-13 00:28:59 +0300

    [jmdb] Forcing a create of the target dir

--- ci-start-celery.sh
+++ ci-start-celery.sh
@@ -3,6 +3,7 @@ cd ureport_project
 
 echo "Starting celery deamon with celery_test_settings.py..."
 
+mkdir target
 nohup ./manage.py celeryd --settings=celery_test_settings --verbosity=2 > target/celery.log &
 
 echo $! > target/celery.pid

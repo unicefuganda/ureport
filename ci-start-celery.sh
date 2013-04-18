43c031c871597e9bced72df9f8dfa34ae4c1bbc0
Jim Barritt <jim@planet-ix.com>
2013-04-18 12:49:55 +0300

    [jmdb] Putting logging verbosity back to 2

--- ci-start-celery.sh
+++ ci-start-celery.sh
@@ -7,7 +7,7 @@ cd ureport_project
 echo "Starting celery deamon with celery_test_settings.py..."
 
 mkdir target
-nohup ./manage.py celeryd --settings=${SETTINGS_FILE} --verbosity=3 > target/celery.log &
+nohup ./manage.py celeryd --settings=${SETTINGS_FILE} --verbosity=2 > target/celery.log &
 
 echo $! > target/celery.pid
 

e63a7d5adc6867c39dd713793542b572d4e3fb05
wanderanimrod <wanderanimrod@gmail.com>
2013-04-23 11:25:53 +0300

    [nim, dj] Updated test to use PageObject models

--- functional_test/scenarios/poll_view_test.py
+++ functional_test/scenarios/poll_view_test.py
@@ -1,22 +1,27 @@
+import time
 from fixtures.create_poll_utils import if_it_exists_delete_poll_called, create_poll_called
-from fixtures.home_page import navigate_to_home_page
+from fixtures.home_page import HomePage
 from nose.tools import assert_equal
-from splinter import Browser
-from fixtures.splinter_wrapper import SplinterTestCase
+from nose.tools import assert_true
+
 
 class TestPollView():
+
+    @classmethod
+    def setUpClass(cls):
+        cls.home_page = HomePage().load()
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.home_page.close_page()
 
     def setUp(self):
         if_it_exists_delete_poll_called("PollViewTest.poll_1")
         create_poll_called("PollViewTest.poll_1")
 
-    def tearDown(self):
-        pass
-
     def test_that_at_least_one_poll_is_shown_on_homepage(self):
-        # home_page = navigate_to_home_page()
-        # polls = home_page.get_list_of_polls()
-
-        # assert_that(polls.size() is(greaterThanOrEqualTo(1)))
-        # assert_equal(1, len(polls))
         assert_equal(1, 1)
+
+    def test_that_poll_list_is_present(self):
+        assert_true(self.home_page.is_poll_list_present())

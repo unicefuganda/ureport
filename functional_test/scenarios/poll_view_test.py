10d083c966df28204bf2803649e08644fc359896
wanderanimrod <wanderanimrod@gmail.com>
2013-05-06 09:55:56 +0300

    [nim] Renaming test and adding Pagination limit

--- functional_test/scenarios/poll_view_test.py
+++ functional_test/scenarios/poll_view_test.py
@@ -1,25 +1,26 @@
 from nose.tools import assert_equal
 from nose.tools import assert_true
 
-from fixtures.create_polls_for_view_poll import  create_and_start_polls
-from fixtures.home_page import HomePage, UreportApplication
+from fixtures.create_polls_for_view_poll import create_and_start_polls
+from fixtures.home_page import UreportApplication
+from ureport_project import settings
 
 
 class TestPollView():
 
     def setUp(self):
-        self.ureport_application = UreportApplication()
+        self.application = UreportApplication()
 
     def tearDown(self):
-        self.ureport_application.close()
+        self.application.close()
 
-    def test_that_there_should_be_only(self):
-        expected_number_of_polls = 10 # TODO load this from the settings
+    def test_that_there_should_be_only_a_limited_number_of_polls_loaded_on_home_page(self):
+
+        expected_number_of_polls = settings.PAGINATION_LIMIT
 
         create_and_start_polls(expected_number_of_polls + 1)
+        home_page = self.application.navigate_to_home_page()
 
-        home_page = self.ureport_application.navigate_to_home_page()
-
-        assert_equal(expected_number_of_polls, home_page.number_of_previous_polls())
+        assert_equal(expected_number_of_polls, home_page.get_number_of_previous_polls())
 
 

34561c93f9b582e7c70c432adb720b19a2e8ad78
wanderanimrod <wanderanimrod@gmail.com>
2013-05-06 14:35:05 +0300

    [nim] Making test use its own settings and not depend on the however the settings files are configured

--- functional_test/scenarios/poll_view_test.py
+++ functional_test/scenarios/poll_view_test.py
@@ -3,15 +3,17 @@ from nose.tools import assert_true
 
 from fixtures.create_polls_for_view_poll import create_and_start_polls
 from fixtures.home_page import UreportApplication
-from ureport_project import settings
+from django.conf import settings
 
 
 class TestPollView():
 
     def setUp(self):
         self.application = UreportApplication()
+        self.set_pagination_limit()
 
     def tearDown(self):
+        self.clear_settings()
         self.application.close()
 
     def test_that_there_should_be_only_a_limited_number_of_polls_loaded_on_home_page(self):
@@ -23,4 +25,11 @@ class TestPollView():
 
         assert_equal(expected_number_of_polls, home_page.get_number_of_previous_polls())
 
+    def set_pagination_limit(self):
+        setattr(settings, 'PAGINATION_LIMIT', 10)
 
+    def clear_settings(self):
+        try:
+            return delattr(settings, 'PAGINATION_LIMIT')
+        except AttributeError:
+            pass

807d5c69cc0381f60c4b89fd7952226ab8c39a38
James Muranga <jmured@gmail.com>
2014-01-21 09:20:45 +0000

    [@Vivitc,MJ] Making sure running functional tests does not require the db to be setup

--- ureport_project/functional_test_settings.py
+++ ureport_project/functional_test_settings.py
@@ -1,5 +1,5 @@
 from ci_settings import *
-
+TEST_RUNNER = 'runner.NoDbTestRunner'
 #If you just want to see the coverage for the functional tests '--cover-erase' - otherwise it adds to the unit test
 NOSE_ARGS = ('--with-xunit', '--xunit-file=target/reports/functional-test/nosetests.ureport.xml', 
              '--with-coverage', '--cover-html', '--cover-html-dir=target/reports/functional-test/coverage', '--cover-package=ureport_project,poll,uganda_common,unregister,message_classifier,contact')
